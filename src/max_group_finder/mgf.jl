6a8ee96978f17aeb92d3c91c0355bdad10301aa2
Gabriel Trettel <gabrielmtrettel@gmail.com>
2019-09-02 22:39:19 -0300

    STUPID performance improvement

--- src/max_group_finder/mgf.jl
+++ src/max_group_finder/mgf.jl
@@ -4,24 +4,25 @@ n_of_workers_available = parse(Int64, ARGS[1])
 addprocs(n_of_workers_available)
 n_of_workers_available-=1
 
-@everywhere import Base.Iterators.partition
 @everywhere include("walker.jl")
+@everywhere import Base.Iterators.partition
 @everywhere using .MaxGrpFinder
 @everywhere using ProgressMeter
 
 function runner()
     n_of_workers_available = parse(Int64, ARGS[1])
-    in_folder = ARGS[2]
-    out_folder = ARGS[3]
+    in_folder   = ARGS[2]
+    out_folder  = ARGS[3]
+    author_list = ARGS[4]
 
-    files = intersect(Set(readdir(in_folder)), Set(map(x->string(strip(x)), readlines(ARGS[4]))))
+    files = intersect(Set(readdir(in_folder)), Set(map(x->string(strip(x)), readlines(author_list))))
     setdiff!(files, Set(readdir(out_folder)))
+
     files = collect(files)
 
     @show chunk_size = ceil(length(files) / n_of_workers_available) |> Int64
 
     println(n_of_workers_available)
-
 
     iter = 0
     total = length(files)
@@ -38,12 +39,8 @@ function runner()
     functions = [consummer for _ in 1:n_of_workers_available]
     chunk_for_each_worker  = map(x->[in_folder, out_folder, channel, x], collect(partition(files, chunk_size)))
 
-
     pmap((fun,inputs)->fun(inputs...), functions, chunk_for_each_worker)
     put!(channel, false)
-
 end
 
 @time runner()
-
-setdiff!(Set([1,2,3,4]), Set([1,2,6,7]))
--- src/max_group_finder/walker.jl
+++ src/max_group_finder/walker.jl
@@ -45,20 +45,29 @@ function verify_comb(author::Author, n::Integer)
     return biggest_walk, n_of_biggest_walks
 end
 
+function check_validity(author::Author, all_ns)
+    for n in all_ns
+        if !haskey(author.groups, n) continue end
+
+         # This ammount takes about 20 minutes to run, so NFW
+        if binomial(length(author.groups[n]),n) >= 51_971_283
+            return nothing
+        end
+    end
+    return true
+end
 
 function walk(author::Author)
     walk_txt = "n_combinations\tbiggest_walk\tn_of_groups_in_tbiggest_walk\n"
     # println("\t\33[37m Parsing $(author.cnpq)")
-    for n in 1:9
+    all_ns = 1:9
+    if check_validity(author, all_ns) == nothing return nothing end
+
+
+    for n in all_ns
         biggest_walk, n_of_biggest_walks = (0,0)
 
         if haskey(author.groups, n)
-            total = (binomial(length(author.groups[n]),n))
-            if total >= 51_971_283 # This ammount takes about 20 minutes to run, so NFW
-                # println("\t\33[37m p=$(length(author.groups[n])) n=$n \t comb=$total\t")
-                return nothing
-            end
-
             biggest_walk, n_of_biggest_walks = verify_comb(author, n)
         end
 
@@ -80,11 +89,10 @@ function consummer(in_folder::String, out_folder::String, ch::RemoteChannel, fil
 
     for author in authors
         already_parsed+=1
-        # println("\33[35m Parsing $already_parsed/$total")
+
         put!(ch, true)
         steps = walk(author)
         if steps == nothing
-            # println("\t\33[31m Too hard to parse, sorry...")
             to_hard_to_parse *= author.cnpq * "\n"
             continue
         end
@@ -95,14 +103,13 @@ function consummer(in_folder::String, out_folder::String, ch::RemoteChannel, fil
 
     if length(to_hard_to_parse) > 0
         i = 0
-        file_name = "to_hard_to_parse"*string(i)
+        file_name = "to_hard_to_parse-"*string(i)
         while isfile(file_name)
             i+=1
             file_name = filename[1:end-1]*string(i)
         end
         file_name*=".list"
-        io = open(file_name)
-        write(io, to_hard_to_parse); close(io)
+        write(file_name, to_hard_to_parse)
     end
 end
 
